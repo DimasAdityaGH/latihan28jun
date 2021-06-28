77e8d5d8f513a87c0861f3c6aa2ef7359eb18ba8
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-28 20:25:33 +0700

    menambahkan variabel const multi

--- const.go
+++ const.go
@@ -5,4 +5,10 @@ import "fmt"
 func main () {
 	const nama = "esa dimas aditya"
 	fmt.Println(nama)
+
+	const (
+		firstName = "dimas"
+		lastName = "aditya"
+	)
+	fmt.Println(firstName, lastName)
 }
