745f6e519ae634d7e1d6328685db462f31219eda
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-28 20:24:24 +0700

    menambahkan file string dan const

---
+++ const.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main () {
+	const nama = "esa dimas aditya"
+	fmt.Println(nama)
+}
---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main () {
+	var name string = "dimas aditya"
+	fmt.Println(name)
+
+	var city = "lamongan"
+	fmt.Println(city)
+}
